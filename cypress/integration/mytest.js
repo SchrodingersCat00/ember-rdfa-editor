b4b719908bcdb9b19b8af44605a57ac11162977a
Oscar Rodriguez <oscar@verify.as>
2020-07-17 18:22:44 +0200

    fixed eslint

--- cypress/integration/mytest.js
+++ cypress/integration/mytest.js
@@ -1,13 +1,13 @@
 describe('Delete handler test', () => {
   it('Visit', () => {
     cy.visit('http://localhost:4200');
-    cy.contains('Dummy')
-  })
+    cy.contains('Dummy');
+  });
   it('Delete letters works', () => {
     cy.visit('http://localhost:4200');
-    cy.get('body').type('Capybaras')
-    cy.get('body').type('{backspace}')
-    cy.get('div[contenteditable]').should('have.text', 'Capybara')
-  })
-})
+    cy.get('body').type('Capybaras');
+    cy.get('body').type('{backspace}');
+    cy.get('div[contenteditable]').should('have.text', 'Capybara');
+  });
+});
 
